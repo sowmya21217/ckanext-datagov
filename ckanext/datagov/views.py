fafc793855e5b1b62349bcf1d6ca1f624dc41dcb
sowmya21217 <root@datagov-s-app01.staging.ifad.org>
2024-10-25 13:31:17 +0200

    add routes

--- ckanext/datagov/views.py
+++ ckanext/datagov/views.py
@@ -3,10 +3,11 @@ from flask import Blueprint, render_template
 
 datagov = Blueprint("datagov", __name__)
 
+
+@datagov.route('/projects')
+
 def projects():
     return render_template("projects.html")
-
-datagov.add_url_rule("/projects", view_func=projects)
 
 
 def get_blueprints():
