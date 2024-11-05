78bf8119701cbc7331231ae58bd5aec5c4e4286d
sowmya21217 <root@datagov-s-app01.staging.ifad.org>
2024-11-05 22:28:07 +0100

    adding views

--- ckanext/datagov/views.py
+++ ckanext/datagov/views.py
@@ -1,42 +1,15 @@
-import logging
-from flask import Blueprint, render_template, redirect, abort, jsonify
-import ckan.plugins as p
-from ckan.plugins.toolkit import config
+from flask import Flask, Blueprint, render_template, send_from_>
 
-# Initialize logging
-log = logging.getLogger(__name__)
+if __name__ == '__main__':
+    datagov.run(debug=True)
 
-# Create a Blueprint for your datagov routes
-datagov_bp = Blueprint('datagov', __name__)
+datagov_ifad = Blueprint("datagov", __name__)
+
+
+@datagov_ifad.route('/projects/')
 
 def projects():
-    """
-    Render the projects page.
-    """
-    try:
-        return render_template('projects/index.html')  # Path to your template
-    except Exception as e:
-        log.error(f"Error rendering projects page: {e}")
-        abort(500, "Internal Server Error: Unable to render the projects page.")
+      return render_template('projects/index.html')
 
-def redirect_homepage():
-    """
-    Redirect to the CKAN dataset homepage.
-    """
-    CKAN_SITE_URL = config.get("ckan.site_url")
-    return redirect(f"{CKAN_SITE_URL}/dataset/", code=302)
-
-def get_popular_count():
-    """
-    Return a JSON response indicating a lack of functionality.
-    """
-    return jsonify({"error": "Functionality to retrieve popular package counts is not implemented."}), 501
-
-# Define your routes
-datagov_bp.add_url_rule('/projects/', view_func=projects)
-datagov_bp.add_url_rule('/', view_func=redirect_homepage)  # Redirect to homepage
-datagov_bp.add_url_rule("/datagov/get-popular-count", methods=['GET'], view_func=get_popular_count)
-
-# Function to return blueprints
 def get_blueprints():
-    return [datagov_bp]  # Return the list of blueprints
+    return [datagov]
