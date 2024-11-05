067eb7d75cbf4430aba06a97ea57e6ce490a8cf0
sowmya21217 <root@datagov-s-app01.staging.ifad.org>
2024-11-05 14:53:17 +0100

    change in views.py

--- ckanext/datagov/views.py
+++ ckanext/datagov/views.py
@@ -7,12 +7,7 @@ datagov = Blueprint("datagov", __name__)
 @datagov.route('/projects')
 
 def projects():
-    try:
       return send_from_directory('templates/projects', 'index.html')
-    
-    except Exception as e:
-        # Handle the error gracefully, perhaps log it or return a different page
-        abort(404)
 
 def get_blueprints():
     return [datagov]
